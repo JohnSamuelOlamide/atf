b6a6bc9cca8055e4713528434e3af7aeca776795
JohnSamuelOlamide <samueljohnolamide@gmail.com>
2024-12-19 13:35:00 +0100

    page loader

--- js/loader.js
+++ js/loader.js
@@ -1,3 +1,10 @@
-  $(window).load(function() {
-    $('#loading').hide();
-  });
+window.addEventListener("load", function () {
+  // Hide loader after all images and content are loaded
+  const loader = document.getElementById("loader");
+  setTimeout(() => {
+    loader.style.opacity = "0"; // Remove from view
+  }, 500); // Matches fade-out duration (optional)
+  setTimeout(() => {
+    loader.style.display = "none"; // Remove from view
+  }, 1000); // Matches fade-out duration (optional)
+});
